6f31f6e6b848960881e599f911ff8f04907c95f4
Evgeniy Khramkov <ewgeniux@gmail.com>
2017-04-18 13:09:31 +0300

    release_upload request

--- lib/fastlane/plugin/mobile_center/actions/release_action.rb
+++ lib/fastlane/plugin/mobile_center/actions/release_action.rb
@@ -1,29 +1,59 @@
 module Fastlane
   module Actions
     class ReleaseAction < Action
-      def self.connection(owner_name, app_name)
+      def self.connection(upload_url = false)
         require 'faraday'
         require 'faraday_middleware'
 
-        UI.message("Creating connection to v0.1/apps/#{owner_name}/#{app_name}/release_uploads")
-
         options = {
-          url: "https://api.mobile.azure.com/v0.1/apps/#{owner_name}/#{app_name}/release_uploads"
+          url: upload_url ? upload_url : "https://api.mobile.azure.com/v0.1"
         }
-        
+
         Faraday.new(options) do |builder|
-          builder.request :multipart
-          builder.request :url_encoded
+          if upload_url
+            builder.request :multipart
+            builder.request :url_encoded
+          else
+            builder.request :json
+          end
           builder.response :json, content_type: /\bjson$/
           builder.use FaradayMiddleware::FollowRedirects
           builder.adapter :net_http
         end
       end
 
+      def self.release_upload(api_token, owner_name, app_name)
+        connection = self.connection
+
+        connection.post do |req|
+          req.url("/v0.1/apps/#{owner_name}/#{app_name}/release_uploads")
+          req.headers['X-API-Token'] = api_token
+          req.body = {}
+        end
+      end
+
+      def self.upload(api_token, file, upload_id, upload_url)
+        connection = self.connection(upload_url)
+
+        options = {}
+        options[:upload_id] = upload_id
+        options[:file] = Faraday::UploadIO.new(file, 'application/octet-stream') if file and File.exist?(file)
+
+        connection.post do |req|
+          req.headers['X-HockeyAppToken'] = api_token
+          req.body = options
+        end
+      end
+
       def self.run(params)
         UI.message("Running release action")
-        connection = self.connection(params[:owner_name], params[:app_name])
-        puts connection
+
+        res = self.release_upload(params[:api_token], params[:owner_name], params[:app_name])
+        upload_id = res.body['upload_id']
+        upload_url = res.body['upload_url']
+
+        res = self.upload(params[:api_token], params[:file], upload_id, upload_url)
+        puts res.body
       end
 
       def self.description
@@ -63,8 +93,8 @@ module Fastlane
                                   optional: false,
                                       type: String),
 
-          FastlaneCore::ConfigItem.new(key: :ipa,
-                                  env_name: "MOBILE_CENTER_DISTRIBUTE_IPA_FILE",
+          FastlaneCore::ConfigItem.new(key: :file,
+                                  env_name: "MOBILE_CENTER_DISTRIBUTE_FILE",
                                description: "Build release path",
                                   optional: false,
                                       type: String),
